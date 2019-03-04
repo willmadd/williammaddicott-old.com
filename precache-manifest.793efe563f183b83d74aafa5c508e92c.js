a84cf2bf56663a155161e2ba643db7f34a5ccceb
William Maddicott <williammaddicott@Williams-MacBook-Pro.local>
2019-03-04 20:41:41 +0000

    Updates

---
+++ precache-manifest.793efe563f183b83d74aafa5c508e92c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ac3c3d3ddea7fc0b9d16",
+    "url": "/static/js/main.ac3c3d3d.chunk.js"
+  },
+  {
+    "revision": "e3c84fa76c44a24c6e52",
+    "url": "/static/js/2.e3c84fa7.chunk.js"
+  },
+  {
+    "revision": "ac3c3d3ddea7fc0b9d16",
+    "url": "/static/css/main.c3ac7ad5.chunk.css"
+  },
+  {
+    "revision": "98db0606e0b06c7fad6f27d02be65c9c",
+    "url": "/index.html"
+  }
+];
--- src/database/getData.js
+++
@@ -1,5 +0,0 @@
-const axios = require("axios");
-
-exports.getData=()=>{
-  return axios.get('https://www.williammaddicott.com/test-api.php')
-}
